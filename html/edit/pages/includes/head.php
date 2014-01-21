e8a958a16199d9be0e8dac16a67832ec46c1ffc3
Bryan Buchanan <bryan@resen.co>
2014-01-20 19:58:29 -0800

    Removed a bunch of unnecessary stuff

--- html/edit/pages/includes/head.php
+++ html/edit/pages/includes/head.php
@@ -4,11 +4,13 @@
 
 	<meta charset="utf-8">
 	<meta name="robots" content="noindex">
+	
+	<title>Edit</title>
 
 	<link href="scss/precedents.scss" rel="stylesheet">
 	<link href="scss/layout.scss" rel="stylesheet">
 	<link href="scss/custom.scss" rel="stylesheet">
-	<link href='http://fonts.googleapis.com/css?family=Lato:100,300,900' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Lato:100,300,900" rel="stylesheet" type="text/css">
 
 	<link href="library/codemirror/lib/codemirror.css" rel="stylesheet">
 	<link href="library/codemirror/theme/neat.css" rel="stylesheet">
