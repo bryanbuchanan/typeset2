9d201d4537be050954e0db60b4a87fb6d4eac135
Bryan Buchanan <bryan@resen.co>
2014-01-20 20:26:16 -0800

    debugging github pushing

--- html/edit/actions/pull.php
+++ html/edit/actions/pull.php
@@ -1,7 +1,10 @@
 <?
-if (!empty($_POST['payload'])):
+if (isset($_POST['payload'])):
 	exec('git pull', $output);
 	$output = print_r($output, true);
 	mail("bryan@resen.co", "typset demo push", $output);
+else:
+	$output = print_r($_POST, true);
+	mail("bryan@resen.co", "typset demo didn't push", $output);
 endif;
 ?>
