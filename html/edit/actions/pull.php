c81a4e268be08427fd24e1f4da629c787146f61c
Bryan Buchanan <bryan@resen.co>
2014-01-20 20:59:47 -0800

    pull script debugging

--- html/edit/actions/pull.php
+++ html/edit/actions/pull.php
@@ -3,8 +3,10 @@ if (isset($_POST['payload'])):
 	exec('git pull', $output);
 	$output = print_r($output, true);
 	mail("bryan@resen.co", "typset demo push", $output);
-else:
+elseif (isset($_POST)):
 	$output = print_r($_POST, true);
 	mail("bryan@resen.co", "typset demo didn't push", $output);
+else:
+	echo "no data";
 endif;
 ?>
