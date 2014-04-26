221a6f707de9ba503be6b5bccebd99ff83b0f558
Bryan Buchanan <bryan@resen.co>
2014-04-26 02:08:57 -0700

    Server environment compatibility fix
    Added fallback for lack of PATH_INFO PHP variable

--- html/edit/actions/scss.php
+++ html/edit/actions/scss.php
@@ -1,4 +1,5 @@
 <?
+if (!isset($_SERVER["PATH_INFO"])) $_SERVER["PATH_INFO"] = $_SERVER["ORIG_PATH_INFO"]; // BlueHost fix
 require "../library/scss/scss.inc.php";
 $scss = new scssc();
 $scss->setImportPaths("../styles/");
