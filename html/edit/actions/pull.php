6130a3db9839eba1f0ec064657b458efd27b73ce
Bryan Buchanan <bryan@resen.co>
2015-07-29 14:15:43 -0700

    test

--- html/edit/actions/pull.php
+++ html/edit/actions/pull.php
@@ -1,8 +1,8 @@
 <?
 if (isset($_POST)):
 	exec('git pull', $output);
-	$output = print_r($output, true);
-	mail("bryan@resen.co", "Typeset Repo Pushed", $output);
+	// $output = print_r($output, true);
+	mail("bryan@resen.co", "typeset demo pushed", $output);
 else:
 	echo "no data";
 endif;
