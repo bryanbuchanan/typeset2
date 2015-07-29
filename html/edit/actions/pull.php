7f29ff487bd6e477a8b432f1b47e7a7af07a6910
Bryan Buchanan <bryan@resen.co>
2015-07-29 14:03:54 -0700

    Update email push notification

--- html/edit/actions/pull.php
+++ html/edit/actions/pull.php
@@ -2,7 +2,7 @@
 if (isset($_POST)):
 	exec('git pull', $output);
 	$output = print_r($output, true);
-	mail("bryan@resen.co", "typeset demo pushed", $output);
+	mail("bryan@resen.co", "Typeset Repo Pushed", $output);
 else:
 	echo "no data";
 endif;
