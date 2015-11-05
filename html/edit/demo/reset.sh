293cea6a015b73dd2737024c15932f5eeabab7eb
Bryan Buchanan <bryan@resen.co>
2015-11-05 10:55:32 -0800

    Fixed demo page cron job reset

--- html/edit/demo/reset.sh
+++ html/edit/demo/reset.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mysql -u typeset2_user -ptypeset2_password typeset2 < content.sql
+
+mysql -u typeset2_user -ptypeset2_password typeset2 < /var/www/repos/typeset2/html/edit/demo/content.sql
