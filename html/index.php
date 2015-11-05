7f8562b05db3dab390eb956ba77e646be1645208
Bryan Buchanan <bryan@resen.co>
2015-11-05 11:01:48 -0800

    Gosquared account change

--- html/index.php
+++ html/index.php
@@ -258,14 +258,14 @@ Images are resized client-side before they're uploaded, so large images can be u
 
 <!-- GoSquared Analytics -->
 <? if ($_SERVER['HTTP_HOST'] === "typeset2.resen.co"): ?>
-	<script>
-	  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
-	  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
-	  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
-	  insertBefore(d,q)}(window,document,'script','_gs');
+<script>
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+  insertBefore(d,q)}(window,document,'script','_gs');
 
-	  _gs('GSN-070927-D');
-	</script>
+  _gs('GSN-543047-B');
+</script>
 <? endif ?>
 
 </body>
